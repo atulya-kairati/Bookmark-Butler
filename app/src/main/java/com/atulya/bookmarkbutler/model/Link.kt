ba580602c26d04931cc90d05f10f71309b1083a2
atulya-kairati <atulyakairati@gmail.com>
2023-07-20 11:08:03 +0530

    tag add and remove

--- app/src/main/java/com/atulya/bookmarkbutler/model/Link.kt
+++ app/src/main/java/com/atulya/bookmarkbutler/model/Link.kt
@@ -1,8 +1,33 @@
 package com.atulya.bookmarkbutler.model
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
+
 data class Link(
     val name: String = "",
     val url: String = "",
     val description: String? = null,
-    val tags: MutableList<String> = mutableListOf()
-)
+    val tags: SnapshotStateList<String> = mutableStateListOf()
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Link
+
+        if (name != other.name) return false
+        if (url != other.url) return false
+        if (description != other.description) return false
+        if (tags.toString() != other.tags.toString()) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + url.hashCode()
+        result = 31 * result + (description?.hashCode() ?: 0)
+        result = 31 * result + tags.hashCode()
+        return result
+    }
+}
--- app/src/main/java/com/atulya/bookmarkbutler/ui/pages/addurl/AddLinkPage.kt
+++ app/src/main/java/com/atulya/bookmarkbutler/ui/pages/addurl/AddLinkPage.kt
@@ -1,10 +1,16 @@
 package com.atulya.bookmarkbutler.ui.pages.addurl
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -21,13 +27,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.atulya.bookmarkbutler.model.Link
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun AddLinkPage(
     modifier: Modifier = Modifier
 ) {
 
-    var link by remember { mutableStateOf(Link(tags = mutableListOf(""))) }
+    var link by remember { mutableStateOf(Link()) }
     var tempTag by remember { mutableStateOf("") }
 
     Surface(
@@ -68,13 +74,39 @@ fun AddLinkPage(
 
             OutlinedTextField(
                 value = tempTag,
-                label = { Text("Description (Optional)") },
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                label = { Text("Enter tags") },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = {
+                    link.tags.add(tempTag)
+                    tempTag = ""
+                }),
                 onValueChange = { text ->
                     tempTag = text
                 },
                 modifier = Modifier.fillMaxWidth()
             )
+
+            FlowRow(modifier = Modifier.padding(8.dp)) {
+                link.tags.forEach { tag ->
+                    Card(
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.primary
+                        ),
+                        modifier = Modifier.padding(2.dp)
+                    ) {
+
+                        Text(
+                            tag,
+                            modifier = Modifier
+                                .padding(vertical = 2.dp, horizontal = 4.dp)
+                                .clickable {
+                                    link.tags.remove(tag)
+                                }
+                        )
+
+                    }
+                }
+            }
         }
     }
 }
